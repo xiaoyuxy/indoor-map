43fff7571e0f322252ed5c04b2b79a8825afad88
liangxiaoyu <liangxiaoyu828@gmail.com>
2017-04-17 23:56:39 -0700

    add the navigate and alert

--- app/src/main/java/com/example/xiaoyuliang/map/MapsActivity.java
+++ app/src/main/java/com/example/xiaoyuliang/map/MapsActivity.java
@@ -1,17 +1,27 @@
 package com.example.xiaoyuliang.map;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,15 +31,19 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
-    private int room = 0;
-    private ArrayList<HashMap<String, String>> locations = new ArrayList<>();
+    private static final int ACCESS_FINE_LOCATION = 1111;
+    private double curlatitude;
+    private double curlogitude;
+    private Intent callIntent;
+    private String number = "4089242222";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +53,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
 
@@ -58,24 +71,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sjsu = new LatLng(37.33642715101153, -121.8819272518158);
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(19));
-        mMap.addMarker(new MarkerOptions().position(sjsu).title("SJSU Engineering Building"));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(190));
+        mMap.addMarker(new MarkerOptions().position(sjsu).title("San Jose State University"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sjsu));
 
         BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.engr_bldg_f3);
 
-         LatLng southWest = new LatLng(37.33639302952647, -121.88194870948792);
-        LatLng northEast = new LatLng(37.33783251621043, -121.88155978918076);
-
-//        LatLng sw = new LatLng(37.33642715101153, -121.8819272518158);
-//        LatLng ne = new LatLng(37.33773228617937, -121.88150882720947);
-//        LatLng nw = new LatLng(37.33727165282733, -121.88254952430725);
-//        LatLng se = new LatLng(37.336904850174776, -121.88091337680817);
-//
-//        LatLngBounds latLngBounds = new LatLngBounds(sw, ne).including(nw).including(se);
-//        //LatLngBounds latLngBounds = new LatLngBounds(southWest, northEast);
         GroundOverlayOptions groundOverlayOptions = new GroundOverlayOptions();
-//        groundOverlayOptions.positionFromBounds(latLngBounds);
+//
 
         LatLng centerLocation = new LatLng(37.337066925986306, -121.88179314136505);
 
@@ -87,46 +90,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         groundOverlayOptions.transparency(0.2f);
         mMap.addGroundOverlay(groundOverlayOptions);
 
-        //LatLng room278 = new LatLng(37.33675983467852, -121.88165943832397);
-        //mMap.addMarker(new MarkerOptions().position(room278).title("Room 390"));
+        LatLng room390 = new LatLng(37.33675983467852, -121.88165943832397);
+        mMap.addMarker(new MarkerOptions().position(room390).title("Room 390"));
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        LatLng pd = new LatLng(37.333181, -121.8800232);
+        mMap.addMarker(new MarkerOptions().position(pd).title("SJSU Police Department"));
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+
+            if (ContextCompat.checkSelfPermission(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                //Location Permission already granted
+                buildGoogleApiClient();
+                googleMap.setMyLocationEnabled(true);
+            }else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        ACCESS_FINE_LOCATION);
+            }
         }
-        mMap.setMyLocationEnabled(true);
-    }
-    public void location() {
-
+        else {
+            buildGoogleApiClient();
+            googleMap.setMyLocationEnabled(true);
+        }
     }
     public void open(View view){
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setMessage("Are you sure you want to call police?");
-                alertDialogBuilder.setPositiveButton("yes",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                call();
-                                Toast.makeText(MapsActivity.this,"Call Police",Toast.LENGTH_LONG).show();
-                            }
+        alertDialogBuilder.setPositiveButton("yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        try {
+                            String uri = "tel:"+number;
+                            callIntent = new Intent(Intent.ACTION_CALL, Uri.parse(uri));
+                            startActivity(callIntent);
+                            Toast.makeText(MapsActivity.this,"Calling Police",Toast.LENGTH_LONG).show();
+                        }catch(Exception e) {
+                            Toast.makeText(getApplicationContext(),"Your call has failed...",
 
-                            private void call() {
-                                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                                callIntent.setData(Uri.parse("tel:0377778888"));
-                                if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.CALL_PHONE
-                                ) != PackageManager.PERMISSION_GRANTED) {
-                                    return;
-                                }
-                                startActivity(callIntent);
-                            }
-
-                        });
+                                    Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
+                    }
+                });
 
         alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
             @Override
@@ -139,4 +147,100 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
     }
+    public void locate(View view){
+
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Follow this route to SJSU Police office");
+        alertDialogBuilder.setPositiveButton("go",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mMap.addPolyline(new PolylineOptions()
+                                .add(new LatLng(curlatitude, curlogitude), new LatLng(37.333181, -121.8800232))
+                                .width(5)
+                                .color(Color.GREEN));
+                        Toast.makeText(MapsActivity.this,"Follow the Green Line",Toast.LENGTH_LONG).show();
+                    }
+
+                });
+
+        alertDialogBuilder.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MapsActivity.this,"Cancel",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    GoogleApiClient mGoogleApiClient;
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+    LocationRequest mLocationRequest;
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        curlatitude = location.getLatitude();
+        curlogitude = location.getLongitude();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    if (ContextCompat.checkSelfPermission(this,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        //Location Permission already granted
+                        buildGoogleApiClient();
+                        mMap.setMyLocationEnabled(true);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+
 }
